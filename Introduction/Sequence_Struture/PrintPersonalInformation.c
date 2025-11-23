13ad577912c93670bb0e75f37a3ec1490353ac98
Steve <falecomstevevaz@gmail.com>
2025-11-23 20:19:51 -0300

    update files and folders of project

---
+++ Introduction/Sequence_Struture/PrintPersonalInformation.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+
+    int name, DOB, mobile;
+    printf("\nName\t: Alexandra Abramov\n");
+    printf("DOB\t: July 14, 1975\n");
+    printf("Mobile\t: 99-9999999999\n");
+    
+    return 0;
+}
